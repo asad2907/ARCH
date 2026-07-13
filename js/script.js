45c48d9f67cfa4adb115ed0e3f01e3170260b339
asad2907 <iaesad29@gmail.com>
2026-07-13 16:54:30 +0000

    c

--- js/script.js
+++ js/script.js
@@ -81,7 +81,8 @@ document.addEventListener('click', event => {
 
   if (destination.origin !== window.location.origin) return;
 
-  window.location.href = destination.href;
+  closeMobileMenu();
+window.location.href = destination.href;
 });
 
 // Restore pages returned from the browser's back-forward cache.
